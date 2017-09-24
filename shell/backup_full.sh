80569e0f585ea72899f5d43f4c6426293de2c267
s3b4h <s3b4h@terra.com.br>
2017-09-24 15:43:19 -0300

    Backup full.

--- shell/backup_full.sh
+++ shell/backup_full.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
+echo "Programa de backup full"
+#Autor: Jhoni Vieceli
+#Programa de criação de backup full
+#DATA: 04/02/2008
 echo " "
+
 # criação da pasta
 criar_pasta () {
 DATA=`date +%Y%m%d`
@@ -20,116 +24,42 @@ echo " Diretório $DIR_BACKUP/backup-full-$DATA existe"
 fi
 
 }
-
 dadosfull() {
 
-SRCDIR="/home/s3b4h/teste" #diretórios que serão feitos backup
-
+SRCDIR="/home/s3b4h/teste" #diretórios que serão feito backup
 DSTDIR=$DIR_BACKUP/backup-full-$DATA #diretório de destino do backup
+DATA=`date +%x-%k%M%S` #pega data atual
+TIME_BKCP=+15 #número de dias em que será deletado o arquivo de backup
 
-DATA_BKP=`date +%Y%m%d`
-
-TIME_FIND=-1440 #+xx busca arquivos criados existentes a xx minutos (arquivos que t                                                                                        enham mais de xx minutos)
-#-xx arquivos que tenham sido criados nos últimos xx minutos
-#12 horas = 720 minutos 8horas 480 minutos
-
-TIME_DEL=+7 # dias em que permanecera o backup diferencial armazenado
-
-#criar o arquivo hostname-full-data.tar no diretório de destino
-
-ARQ=$DSTDIR/$HOSTNAME-full-$DATA_BKP.tar
-
+#criar o arquivo full-data.tar no diretório de destino
+ARQ=$DSTDIR/$HOSTNAME-full-$DATA.tar.gz
 #data de inicio backup
-
 DATAIN=`date +%c`
-
-echo " Data de inicio: $DATAIN"
+echo "Data de inicio: $DATAIN"
 
 }
 
-backupfull(){
-
+backupfull() {
 sync
-
-find $SRCDIR ! -path "/home/s3b4h/teste/regulacao/*" ! -path "/home/s3b4h/teste/vendor/*" -exec tar -rvf $ARQ {} ";" # adicionar
-# ! -path os diretorios que não entrarão no backup
+tar -cvf $ARQ $SRCDIR
 
 if [ $? -eq 0 ] ; then
-
-echo "-------------------------------------"
-echo "Backup Full concluído com sucesso"
-
+echo "----------------------------------------"
+echo "Backup Full concluído com Sucesso"
 DATAFIN=`date +%c`
-
 echo "Data de termino: $DATAFIN"
 echo "Backup realizado com sucesso" >> /var/log/backup_full.log
 echo "Criado pelo usuário: $USER" >> /var/log/backup_full.log
 echo "INICIO: $DATAIN" >> /var/log/backup_full.log
 echo "FIM: $DATAFIN" >> /var/log/backup_full.log
-echo "------------------------------------------------" >> /var/log/backup_full                                                                                        encial.log
+echo "-----------------------------------------" >> /var/log/backup_full.log
 echo " "
 echo "Log gerado em /var/log/backup_full.log"
-
 else
-
-echo "ERRO! Backup Diferencial $DATAIN" >> /var/log/backup_full.log
-
+echo "ERRO! Backup do dia $DATAIN" >> /var/log/backup_full.log
 fi
-
-}
-
-procuraedestroifull(){
-
-#apagando arquivos mais antigos (a 7 dias que existe (-cmin +2)
-
-find /backup/ -name "backup-full-*" -ctime $TIME_DEL -exec rm -rf {} ";"
-
-if [ $? -eq 0 ] ; then
-
-echo "Arquivo de backup mais antigo eliminado com sucesso!"
-
-else
-
-echo "Erro durante a busca e destruição do backup antigo!"
-
-fi
-
-}
-
-compactar () {
-
-echo "Script de compactação"
-
-DATAIN=`date +%c`
-
-echo "Data de inicio: $DATAIN"
-
-gzip -9 $DSTDIR/*.tar
-
-echo "Compactação concluída"
-
-DATAFIN=`date +%c`
-
-echo "Data de termino: $DATAFIN"
-echo "Compactação concluída"
-echo "INICIO: $DATAIN" >> /var/log/backup_compactacao.log
-echo "FIM: $DATAFIN" >> /var/log/backup_compactacao.log
-echo "Realizado pelo usuário: $USER" >> /var/log/backup_compactacao.log
-echo "-----------------------------------" >> /var/log/backup_compactacao.log
-echo "Log gerado em /var/log/backup_compactacao.log"
-
 }
 
 criar_pasta
-
 dadosfull
-
 backupfull
-
-procuraedestroifull
-
-sleep 2
-
-compactar
-
-exit 0
