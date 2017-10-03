b899f61d5d33f09997d15d3b2f395c6b65b79ecf
Sebastião José da Silva Junior <sjunior@stspo.local>
2017-10-03 10:41:45 -0300

    Configuração do log

--- python/projeto_backup/backup.py
+++ python/projeto_backup/backup.py
@@ -23,7 +23,37 @@ def parse_ambiente():
     return var_amb
 
 
+def inicio(horaInicio):
+    inicio = "BACKUP FULL DO FILESERVER INICIADO AS %s" % horaInicio
+    return inicio
+
+
+def termino(diaInicio, horaInicio, pathlog):
+    hoje = (time.strftime("%d-%m-%Y"))
+    horaFinal   = time.strftime('%H:%M:%S')
+    final = '''
+  ===========================================================================
+                            BACKUP FULL FINALIZADO
+
+                HORA INICIAL:    %s  -  %s
+                HORA FINAL  :    %s  -  %s
+                LOG FILE    :    %s
+  ===========================================================================
+
+    ''' % (diaInicio, horaInicio, hoje, horaFinal, pathlog)
+    return final
+
+
+def geralog():
+    logfile     = 'backup-full.txt'
+    pathlog     = '/var/log/%s' % logfile
+
+    return pathlog
+
+
+
 def executa_backup():
+    #pega informacoes do arquivo para montar o tar
     hostname = socket.gethostname()
     date = (time.strftime("%Y%m%d"))
     arquivo = '%s-%s-full.tar.gz' % (hostname, date)
@@ -32,12 +62,14 @@ def executa_backup():
     dir_origem = config.get("diretorios", "DIR_ORIGEM")
     dir_destino = config.get("diretorios", "DIR_DESTINO") + "/" + date + "/" + "APP/"
 
+    #verifica se pasta ja existe se nao existir cria
     if os.path.isdir(dir_destino):
         print ('Ja existe uma pasta com esse nome!')
     else:
         os.makedirs(dir_destino)
         print ('Pasta criada com sucesso!')
 
+    #exclusao de pastas do tar
     exclude_dir = config.get("diretorios","EXCLUDE_DIR")
     if exclude_dir == '':
         backup = 'tar cvf %s %s' %(dir_destino + arquivo, dir_origem)
@@ -51,12 +83,30 @@ def executa_backup():
     
         print (backup)
     return backup
-
+    
 
 def main():
+    #inicio do log
+    horaInicio  = time.strftime('%H:%M:%S')
+    pathlog     = geralog()
     exec_backup = executa_backup()
-
-    subprocess.call(exec_backup, shell=True)
-
+    log         = ' >> %s' % pathlog
+    start       = inicio(horaInicio)
+    
+    l = open(pathlog, 'w')
+    l.write(start)
+    l.close()
+    
+    
+    #executa backup
+    subprocess.call(exec_backup + log, shell=True)
+    
+    #fim do backup
+    diaInicio   = (time.strftime("%d-%m-%Y"))
+    final       = termino(diaInicio, horaInicio, pathlog)
+    r           = open(pathlog, 'w')
+    r.write(final)
+    r.close()
+    
 
 main()
