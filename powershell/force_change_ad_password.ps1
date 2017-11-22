6a755588f3181a7c3df87d1f3753e0e7784f3be3
Sebastião Junior <sebastiao.junior@UNITELCO.COM.BR>
2017-11-22 11:36:33 -0200

    add powershell scripts

---
+++ powershell/force_change_ad_password.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module ActiveDirectory
+
+$users = Get-Content C:\Script\usuarios.txt
+
+foreach($user in $users)
+
+{
+
+Get-ADUser $user | Set-ADUser -ChangePasswordAtLogon $true
+
+Write-Host "Usuario $user forçado mudar senha no proximo logon"
+
+}
